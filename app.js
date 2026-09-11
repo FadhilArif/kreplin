20b1b172dec9d05bcb6c4737f504beedc6f1eac1
FadhilArif <125202848+FadhilArif@users.noreply.github.com>
2026-09-11 18:09:57 +0600

    new model v3.1

--- app.js
+++ app.js
@@ -118,16 +118,16 @@
 
   const $ = (id) => document.getElementById(id);
 
-  const views = {
-    landing: $('landingView'),
-    auth: $('authView'),
-    dashboard: $('dashboardView'),
-    instruction: $('instructionView'),
-    test: $('testView'),
-    result: $('resultView'),
-    history: $('historyView'),
-    showView('admin');
-  };
+const views = {
+  landing: $('landingView'),
+  auth: $('authView'),
+  dashboard: $('dashboardView'),
+  instruction: $('instructionView'),
+  test: $('testView'),
+  result: $('resultView'),
+  history: $('historyView'),
+  admin: $('adminView'),
+};
 
   const state = {
     session: null,
